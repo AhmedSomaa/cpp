bf0b6a4603042504d46e3baf4a0cc577a552f2e5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-06-11 14:03:01 +0200

    Add first program

---
+++ src/0-basics/0-first-program.cpp
@@ -0,0 +1,16 @@
+// A simple program to display "Hello World"
+
+// header file for I/O functions
+#include <iostream>     
+using namespace std;
+
+/* 
+ * main function -
+ * where the execution of the program begins
+ */
+int main(){
+    // print Hello World
+    cout << "Hello World\n";
+    return 0;
+}
+
